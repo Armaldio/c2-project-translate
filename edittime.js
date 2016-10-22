75e234fba31ba528673fa60261d2fea5c205e5df
Quentin Goinaud <Quentin Goinaud>
2016-10-22 15:09:40 +0200

    v1.0

---
+++ edittime.js
@@ -0,0 +1,105 @@
+﻿function GetPluginSettings() {
+	return {
+		"name"       : "Project Translation",
+		"id"         : "armaldio_project_translate",
+		"version"    : "1.0",
+		"description": "",
+		"author"     : "Armaldio",
+		"help url"   : "",
+		"category"   : "Web",
+		"type"       : "object",			// not in layout
+		"rotatable"  : false,
+		"flags"      : pf_singleglobal
+	};
+}
+
+//////////////////////////////////////////////////////////////
+// Conditions
+//AddCondition(0, 0, "Cookies enabled", "Browser", "Cookies are enabled", "Browser has cookies enabled.", "CookiesEnabled");
+AddCondition(0, cf_trigger, "Languages loaded", "Languages", "On Languages loaded", "Trigger when languages are succesfully loaded", "OnLoadSuccess");
+
+//AddStringParam("Content", "The JSON to import");
+AddCondition(1, cf_none, "Languages are cached", "Languages", "If language are available offline", "Check if languages are available offline", "IsCached");
+
+AddCondition(2, cf_none, "Cache is outdated", "Languages", "If chache outdated", "Check if local cache is outdated", "OutdatedCache");
+
+AddCondition(3, cf_trigger, "Import fail", "Languages", "On Import fail", "When importing from url failed", "OnImportFail");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Cache", "Download files for offline usage");
+AddAction(0, 0, "Load from url", "Load", "Import file", "Import Language list", "Import");
+
+AddAction(1, 0, "Load from cache", "Load", "Load languages from cache", "Load languages from cache", "LoadFromCache");
+AddAction(2, 0, "Remove cache", "Cache", "Remove cache", "Remove cache", "RemoveCache");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+//AddExpression(3, ef_return_number, "Absolute mouse Y", "Cursor", "AbsoluteY", "Get the mouse cursor Y co-ordinate on the canvas.");
+AddExpression(0, ef_return_number, "Get languages number", "Translation", "LanguagesNumber", "Get the available languages number");
+
+AddNumberParam("Index", "The index of the language");
+AddExpression(1, ef_return_string, "Get languages name at index", "Translation", "GetLangAt", "Get language name at specific index");
+
+AddExpression(2, ef_return_number, "Get last index", "Translation", "LastImportedIndex", "Get the language index of the last imported language");
+
+AddStringParam("Language", "Must match languages in the list imported at start");
+AddStringParam("Identifier", "The identifier to identify the string");
+AddExpression(3, ef_return_string, "Get string", "Translation", "GetString", "Get translated string from language and identifier");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text, "Lang list url", "http://", "The url of your language list")
+];
+
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType() {
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType() {
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function (instance) {
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type) {
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type     = type;
+
+	// Set the default property values from the property table
+	this.properties = {};
+
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function () {
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function (property_name) {
+}
+
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function (renderer) {
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function () {
+}
---
+++ runtime.js
@@ -0,0 +1,213 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+var languages = {};
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.armaldio_project_translate = function (runtime) {
+	this.runtime = runtime;
+};
+
+(function () {
+	var pluginProto = cr.plugins_.armaldio_project_translate.prototype;
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function (plugin) {
+		this.plugin  = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function () {
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function (type) {
+		this.type    = type;
+		this.runtime = type.runtime;
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function () {
+		if ((this.remoteurl = this.properties[0]) === "")
+			alert("Remote url not defined ! Pleas configure it in the properties of the plugin");
+	};
+
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections) {
+		propsections.push({});
+	};
+	/**END-PREVIEWONLY**/
+
+	function isCached() {
+		return localStorage.getItem("langcache") !== null;
+	}
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {
+	};
+
+	Cnds.prototype.OnLoadSuccess = function () {
+		console.log("Load success");
+		return true;
+	};
+
+	Cnds.prototype.OnImportFail = function () {
+		return true;
+	};
+
+	Cnds.prototype.IsCached = function () {
+		console.log("lanaguages are offline : ", isCached());
+		return isCached();
+	};
+
+	Cnds.prototype.OutdatedCache = function () {
+		var outdated = false;
+
+		$.ajax({
+			url     : this.remoteurl,
+			async   : false,
+			dataType: "text",
+			success : function (d) {
+				var data = JSON.parse(d);
+				for (var key in data) {
+					if (key === "version") {
+						var remoteVersion = parseFloat(data[key]);
+						console.log("Remote version = ", remoteVersion);
+
+						var localVersion = parseFloat(localStorage.getItem("langversion"));
+						console.log("Local version = ", localVersion);
+
+						if (remoteVersion > localVersion) {
+							console.log("Cache is outdated");
+							outdated = true;
+							break;
+						}
+					}
+				}
+			}
+		});
+		return outdated;
+	};
+
+
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {
+	};
+
+	Acts.prototype.Import = function (cache) {
+		var self = this;
+
+		try {
+
+			$.ajax({
+				url     : this.remoteurl,
+				async   : false,
+				dataType: "text",
+				success : function (d) {
+					var list = JSON.parse(d);
+
+					console.log("list", list);
+
+					for (var langname in list) {
+						if (langname === "version") {
+							localStorage.setItem("langversion", list[langname]);
+						} else if (list.hasOwnProperty(langname)) {
+							var url = list[langname];
+							$.ajax({
+								url     : url,
+								async   : false,
+								dataType: "text",
+								success : function (data) {
+									var langdata        = JSON.parse(data);
+									languages[langname] = langdata;
+								}
+							});
+						}
+					}
+
+					console.log("cache : ", cache, "localstorage", typeof localStorage != 'undefined');
+					if (cache == 1 && typeof localStorage != 'undefined') {
+						console.log("Caching");
+						localStorage.setItem("langcache", JSON.stringify(languages));
+					}
+					self.runtime.trigger(cr.plugins_.armaldio_weblate_translate.prototype.cnds.OnLoadSuccess, self);
+					console.log(languages);
+				}
+			});
+		} catch (e) {
+			console.log("Error Importing : ", e);
+			self.runtime.trigger(cr.plugins_.armaldio_weblate_translate.prototype.cnds.OnLoadSuccess, self);
+		}
+	};
+
+	Acts.prototype.LoadFromCache = function () {
+		var self = this;
+
+		if (isCached()) {
+			var cache = localStorage.getItem("langcache");
+			languages = JSON.parse(cache);
+			console.log("Loading from cache", languages);
+			self.runtime.trigger(cr.plugins_.armaldio_weblate_translate.prototype.cnds.OnLoadSuccess, self);
+		}
+		else {
+			alert("Cannot load languages from cache !");
+		}
+	};
+
+	Acts.prototype.RemoveCache = function () {
+		var self   = this;
+		var remove = localStorage.removeItem("langcache");
+		//TODO error handling
+		console.log("Remove : ", remove);
+	};
+
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {
+	};
+
+	Exps.prototype.LanguagesNumber = function (ret) {
+		console.log("Languages number : ", Object.keys(languages).length);
+		ret.set_int(Object.keys(languages).length);
+	};
+
+	Exps.prototype.GetString = function (ret, lang, identifier) {
+		console.log("a", languages[lang].hasOwnProperty(identifier));
+		if (lang in languages) {
+			if (languages[lang].hasOwnProperty(identifier)) {
+				ret.set_string(languages[lang][identifier]);
+			}
+			else {
+				ret.set_string("[Unknown identifier " + identifier + " ]");
+			}
+		}
+		else
+			ret.set_string("[Unknown language]");
+	};
+
+	Exps.prototype.GetLangAt = function (ret, index) {
+		if (Object.keys(languages)[index])
+			ret.set_string(Object.keys(languages)[index]);
+		else {
+			console.log("Language number " + index + " doen't exixts");
+		}
+	};
+
+	pluginProto.exps = new Exps();
+
+}());
